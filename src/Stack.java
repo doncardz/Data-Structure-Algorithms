69b222b60881b6009b31a0044b272fb92d0d9b52
doncardz <carloventocilla11@gmail.com>
2026-09-04 18:17:26 +0800

    Stacks

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,33 @@
-public class Stack{
+import java.util.Stack;
+
+public class stack{
     public static void main(String[] args){
+
+        //stack = Last-In First-Out, LIFO
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Lenovo");
+        stack.push("Asus");
+        stack.push("Acer");
+        stack.push("MSI");
+
+
+        /*
+            stack.push = add to stack
+            stack.push("MSI");
+
+            stack.pop = hides upper stack
+            stack.pop();
+
+            stack.peek = display the upper stack
+            System.out.println(stack.peek());
+
+            stack.search = finds stack
+            System.out.println(stack.search("MSI"));
+        */
+
+        System.out.println(stack);
         
     }
 }
