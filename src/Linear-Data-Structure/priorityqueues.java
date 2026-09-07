e680376f129bb59bbe7eaa95eb23c702f9a561ea
doncardz <carloventocilla11@gmail.com>
2026-09-07 20:05:03 +0800

    Priority Queue

--- src/Linear-Data-Structure/priorityqueues.java
+++ src/Linear-Data-Structure/priorityqueues.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class priorityqueues {
     public static void main(String[] args) {
 
+        //Priority Queue = A FIFO data structure that serves elements
+        //                  with the highest priorities first
+        //                  before elements with lower priority.
+
+
+        //Queue<String> queue = new PriorityQueue<>(Collections.reverseOrder()); = Highest priority first
+        Queue<String> queue = new PriorityQueue<>();
+
+        queue.offer("A");
+        queue.offer("C");
+        queue.offer("D");
+        queue.offer("B");
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 }
