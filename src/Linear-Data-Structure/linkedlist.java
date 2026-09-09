c80fbb85e07d5ae4a0de213c2ede7c1736f8cb28
doncardz <carloventocilla11@gmail.com>
2026-09-09 20:09:15 +0800

    basics of LinkedList

--- src/Linear-Data-Structure/linkedlist.java
+++ src/Linear-Data-Structure/linkedlist.java
@@ -1,5 +1,22 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 public class linkedlist {
     public static void main(String[] args) {
-        
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        head.next = second;
+        second.next = third;
+
     }
 }
